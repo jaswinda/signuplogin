61b0652205b2eb4f835ccc2dd27ca7dcc6ff99c3
jaswinda <singhjassi2624@gmail.com>
2021-09-06 08:53:07 +0545

    payment integration

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,4 +1,4 @@
-const String root = "http://192.168.1.131/test/";
+const String root = "http://192.168.1.8/test/";
 const String loginApi = "$root/login.php";
 const String signupApi = "$root/signup.php";
 const String logoutApi = "$root/logout.php";
